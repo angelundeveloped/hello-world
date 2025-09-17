04f671121f0b8c24503e898a2057cedf25ffb1c9
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2025-09-17 15:50:07 -0600

    createing a curl and wget tes

---
+++ hello_world.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O script_wget.sh "https://github.com/angelundeveloped/hello_world/blob/branch/path/to/script.py?raw=true"
+
+
+curl -o script_curl.sh https://raw.githubusercontent.com/angelundeveloped/hello_world/branch/path/to/script.sh
