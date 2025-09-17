cd1acead1702b9990923e82aa5df82152130016c
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2025-09-17 15:51:22 -0600

    new commands

--- hello_world.sh
+++ hello_world.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget -O script_wget.sh "https://github.com/angelundeveloped/hello_world/blob/branch/path/to/script.py?raw=true"
+wget -O script_wget.sh "https://github.com/angelundeveloped/hello_world/blob/master/hello_world.sh?raw=true"
 
+curl -o script_curl.sh https://raw.githubusercontent.com/angelundeveloped/hello_world/master/hello_world.sh
 
-curl -o script_curl.sh https://raw.githubusercontent.com/angelundeveloped/hello_world/branch/path/to/script.sh
+wget -O script_wget.sh https://github.com/angelundeveloped/hello-world/blob/master/hello_world.sh
